feb447184531fe9851eff68c163cc6f3e634fbf0
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-04-29 19:54:03 +0430

    refactoring dependencies

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
@@ -16,7 +15,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
