341549032e976879f259c808ca5e8ebf4124c655
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-04-29 20:05:14 +0430

    refactoring code

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const path = require('path');
-
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 
-const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
@@ -18,10 +18,22 @@ const authRoutes = require('./routes/auth');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect(
+  'mongodb://localhost:27017/maxi',
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  ()=>{
+    console.log('connecting db successfully!');
+  }
+);
+
 app.use((req, res, next) => {
   User.find({_id: '5bab316ce0a7c75f783cb8a8'})
     .then(user => {
       req.user = user;
+      console.log(user);
       next();
     })
     .catch(err => console.log(err));
@@ -30,20 +42,8 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    'mongodb://localhost:27017/maxi',
-    { 
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    },
-    ()=>{
-      console.log('connecting db successfully!');
-    }
-  );
-
-
-app.listen(3000 , ()=> console.log('server started'));
+app.listen(PORT , ()=> {
+  console.log(`server started on port : ${PORT}`);
+});
