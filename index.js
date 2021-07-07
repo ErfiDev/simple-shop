a98697c409b4be607bf29a36ce0587a5213a30b5
Erfan <erfanhanifezade@gmail.com>
2021-07-07 21:15:16 +0430

    apply public dir for css and js files

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const App = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8000;
+
+App.set("view engine", "ejs");
+App.use(express.static(path.join(__dirname + "/public")));
+
+App.get("/", (req, res) => {
+  res.render("index", {
+    userStatus: true,
+  });
+});
 
 App.listen(PORT, () => console.log(`running on the ${PORT}`));
