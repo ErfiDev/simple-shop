cfb31faa0dee4abf5942e717e7d88af99a6290f0
Erfan <erfanhanifezade@gmail.com>
2021-07-07 22:38:48 +0430

    add cookie-parser middleWare

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const App = express();
@@ -8,6 +9,7 @@ App.set("view engine", "ejs");
 App.use(express.static(path.join(__dirname + "/public")));
 App.use(express.json({ limit: "25mb" }));
 App.use(express.urlencoded({ extended: true }));
+App.use(cookieParser());
 
 App.use("/", (req, res, next) => {
   const cookies = req.cookies;
